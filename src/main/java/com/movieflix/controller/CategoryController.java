8cbb4a6c5956873d96722716a5c340e435cf09f9
igorpandolf <pandolf.igor@gmail.com>
2026-07-07 19:19:26 -0300

    Category

---
+++ src/main/java/com/movieflix/controller/CategoryController.java
@@ -0,0 +1,51 @@
+package com.movieflix.controller;
+
+import com.movieflix.controller.request.CategoryRequest;
+import com.movieflix.controller.response.CategoryResponse;
+import com.movieflix.entity.Category;
+import com.movieflix.mapper.CategoryMapper;
+import com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<Category> categories = service.findAll();
+        List<CategoryResponse> list = categories.stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList();
+
+        return ResponseEntity.ok(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest request) {
+        Category category = CategoryMapper.toCategory(request);
+        category = service.saveCategory(category);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CategoryMapper.toCategoryResponse(category));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> getByCategoryId(@PathVariable Long id) {
+        return service.findCategoryById(id)
+                .map(category -> ResponseEntity.ok(CategoryMapper.toCategoryResponse(category)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteByCategoryId(@PathVariable Long id) {
+        service.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
--- src/main/java/com/movieflix/entity/Category.java
+++ src/main/java/com/movieflix/entity/Category.java
@@ -1,13 +1,21 @@
 package com.movieflix.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "category")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100, nullable = false)
     private String name;
 }
