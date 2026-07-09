0f8f79ba3e8f199137ecaff670228fed2eb75daf
igorpandolf <pandolf.igor@gmail.com>
2026-07-09 16:52:04 -0300

    User

---
+++ src/main/java/com/movieflix/entity/User.java
@@ -0,0 +1,28 @@
+package com.movieflix.entity;
+
+import com.sun.jdi.PrimitiveValue;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
